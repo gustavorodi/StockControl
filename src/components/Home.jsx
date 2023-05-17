7561112fcece8c1d51b2c80b1d52f87f1aaeecd5
gustavorodi <gustavocaetano.rodrigues@gmail.com>
2023-05-17 00:21:40 -0300

    Estuturando Projeto

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Home() {
+  // Lógica do componente e/ou estado aqui
+
+  return (
+    // JSX para renderizar o componente
+    <div>
+      <h1>Título</h1>
+      <p>Conteúdo do componente</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/theme.jsx
@@ -0,0 +1,15 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+    colors: {
+      brand: {
+        primary: "#F8F8F8",
+        secondary: "#4E4E4E",
+        accent: "#394867",
+        neutral: "#ECECEC",
+        text: "#333333",
+      },
+    },
+  });  
+
+export default theme;
